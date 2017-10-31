82b355970bfdc2eecfeb3e3098721dd327fb8add
nainemom <nainemom@gmail.com>
2017-10-31 17:14:20 +0330

    fix $check function result

--- src/index.js
+++ src/index.js
@@ -19,18 +19,18 @@ module.exports = class {
       this[fieldName] = this.$fields[fieldName].default || undefined
     })
   }
-  _typeOf(obj) {
+  _typeOf (obj) {
     return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
   }
-  $set(obj) {
+  $set (obj) {
     Object.keys(this.$fields).forEach(fieldName => {
-      const defaultValue = typeof this.$fields[fieldName].default === 'undefined'? undefined: this.$fields[fieldName].default
-      if( typeof obj[fieldName] === 'undefined' ){
+      const defaultValue = typeof this.$fields[fieldName].default === 'undefined' ? undefined : this.$fields[fieldName].default
+      if (typeof obj[fieldName] === 'undefined') {
         this[fieldName] = defaultValue
       } else {
         this[fieldName] = obj[fieldName]
       }
-    });
+    })
   }
   $check () {
     const ret = {
@@ -38,8 +38,8 @@ module.exports = class {
       errors: {}
     }
     Object.keys(this.$fields).forEach(fieldName => {
-      const types = typeof this.$fields[fieldName].type === 'undefined'? []: (this._typeOf(this.$fields[fieldName].type) === 'string'? [this.$fields[fieldName].type]: this.$fields[fieldName].type)
-      const value = typeof this[fieldName] === 'undefined'? undefined: this[fieldName]
+      const types = typeof this.$fields[fieldName].type === 'undefined' ? [] : (this._typeOf(this.$fields[fieldName].type) === 'string' ? [this.$fields[fieldName].type] : this.$fields[fieldName].type)
+      const value = typeof this[fieldName] === 'undefined' ? undefined : this[fieldName]
       const validatorValue = typeof this.$fields[fieldName].validator === 'function' ? this.$fields[fieldName].validator(value) : true
 
       let errorMessage
@@ -48,7 +48,8 @@ module.exports = class {
       } else if (typeof this.$fields[fieldName].message === 'string') {
         errorMessage = this.$fields[fieldName].message
       }
-      if( ( types.length > 0 && types.indexOf(this._typeOf(value)) === -1 )  || !validatorValue ){
+      if ((types.length > 0 && types.indexOf(this._typeOf(value)) === -1) || !validatorValue) {
+        ret.result = false
         ret.errors[fieldName] = errorMessage || 'Illegal value!'
       }
     })
@@ -57,8 +58,8 @@ module.exports = class {
   $clean () {
     let ret = true
     Object.keys(this.$check().errors).forEach(fieldName => {
-      const newValue = typeof this.$fields[fieldName].default === 'undefined'? this[fieldName]: this.$fields[fieldName].default
-      if( newValue === this[fieldName] ){
+      const newValue = typeof this.$fields[fieldName].default === 'undefined' ? this[fieldName] : this.$fields[fieldName].default
+      if (newValue === this[fieldName]) {
         ret = false
       } else {
         this[fieldName] = newValue
